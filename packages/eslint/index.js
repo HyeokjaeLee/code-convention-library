2cfcac018ba7a83e52f2f8c7436e97a109e7a469
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-04-30 22:11:21 +0900

    chore: 패키지 매니저 변경
    - yarn berry > pnpm

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:import/recommended',
-    'plugin:promise/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:import/recommended",
+    "plugin:promise/recommended",
     ...[
-      './rules/base',
-      './rules/promise',
-      './rules/import',
-      './rules/react',
-      './rules/jsx-a11y',
+      "./rules/base",
+      "./rules/promise",
+      "./rules/import",
+      "./rules/react",
+      "./rules/jsx-a11y",
     ].map(require.resolve),
   ],
 
@@ -28,26 +28,26 @@ module.exports = {
 
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        ...['./rules/typescript'].map(require.resolve),
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        ...["./rules/typescript"].map(require.resolve),
       ],
       settings: {
         /**
          * @description import plugin with Typescript configuration
          * @link https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration
          */
-        'import/parsers': {
-          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
         },
       },
     },
--- packages/eslint/rules/promise.js
+++ packages/eslint/rules/promise.js
@@ -4,11 +4,11 @@ module.exports = {
      * @description Callback 보다 Async/Await 선호
      * @link https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/prefer-await-to-callbacks.md
      */
-    'promise/prefer-await-to-callbacks': 'error',
+    "promise/prefer-await-to-callbacks": "error",
     /**
      * @description Promise 보다 Async/Await 선호
      * @link https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/prefer-await-to-then.md
      */
-    'promise/prefer-await-to-then': 'off',
+    "promise/prefer-await-to-then": "off",
   },
 };
