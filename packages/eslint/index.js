d82f7fbf212e5a6c207673ad84b797d75be5fdae
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-05-02 00:47:34 +0900

    feat: 패키지 구성 (#1)
    * chore: monorepo 구성
    
    * chore: 패키지 초기 설정
    
    * chore: typescript sdk 설정
    
    * feat: 일부 규칙 수정
    
    - 중첩 삼항 연산자 에러로 변경
    - 단일 라인 조건문 블럭 제한
    
    * feat: default-case 비활성화
    
    * chore: rule 변경
    
    - 패키지 업데이트
    
    * chore: packages 재분류
    
    * remove: git 정리
    
    * feat: eslint 플러그인 분리
    
    * chore: yarn 관련 파일 삭제
    
    * chore: 패키지 매니저 변경
    
    - yarn berry > pnpm
    
    * chore: versioning 스크립트 작성
    
    * chore: stylelint css-in-js 방식 분리
    
    * chore: 패키지 업데이트
    
    * chore: version up
    
    * chore: script 수정
    
    - package 이름 수정

---
+++ packages/eslint/index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:import/recommended",
+    "plugin:promise/recommended",
+    ...[
+      "./rules/base",
+      "./rules/promise",
+      "./rules/import",
+      "./rules/react",
+      "./rules/jsx-a11y",
+    ].map(require.resolve),
+  ],
+
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    jsx: true,
+    useJSXTextNode: true,
+  },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        ...["./rules/typescript"].map(require.resolve),
+      ],
+      settings: {
+        /**
+         * @description import plugin with Typescript configuration
+         * @link https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration
+         */
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+      },
+    },
+  ],
+};
---
+++ packages/stylelint/css-in-js.js
@@ -0,0 +1,8 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      customSyntax: "@stylelint/postcss-css-in-js",
+    },
+  ],
+};
