7ec800c7e025b9f06e345eec41245f550cba5a4c
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-05-03 03:26:08 +0900

    ver: 1.2.0
    - CI/CD 스크립트 버저닝 제외
    - test 환경 구성
    - stylelint 버그 수정

--- packages/stylelint/Index.js
+++ packages/stylelint/Index.js
@@ -1,4 +1,10 @@
 module.exports = {
-  extends: ["stylelint-config-standard", ...["./rules/base"].map(require.resolve)],
+  extends: ["stylelint-config-recommended-scss", ...["./rules/base.js"].map(require.resolve)],
   plugins: ["stylelint-order"],
+  overrides: [
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      customSyntax: "@stylelint/postcss-css-in-js",
+    },
+  ],
 };
--- test/vite.config.ts
+++ test/vite.config.ts
@@ -1,3 +1,4 @@
+/* stylelint-disable-next-line CssSyntaxError */
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
